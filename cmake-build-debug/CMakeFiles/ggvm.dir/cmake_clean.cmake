bd922422c6f141a3a441875797d0c48eebc49bab
Alex <814943412@qq.com>
2020-07-26 05:14:54 +0800

    Update File Structure

---
+++ cmake-build-debug/CMakeFiles/ggvm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ggvm.dir/main.cpp.obj"
+  "ggvm.exe"
+  "ggvm.exe.manifest"
+  "ggvm.pdb"
+  "libggvm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ggvm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
